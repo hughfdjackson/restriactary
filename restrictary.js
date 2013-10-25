0e709b94aadc3f3978e205907e9aec37a6abc160
hugh fd jackson <hughfdjackson@googlemail.com>
2013-10-25 23:30:32 +0100

    removed trailing comma of doom!

--- restrictary.js
+++ restrictary.js
@@ -4,5 +4,5 @@ module.exports = {
     nullary: function(fn){ return function(){ return fn() } },
     unary: function(fn){ return function(a){ return fn(a) } },
     binary: function(fn){ return function(a, b){ return fn(a, b) } },
-    ternary: function(fn){ return function(a, b, c){ return fn(a, b, c) } },
+    ternary: function(fn){ return function(a, b, c){ return fn(a, b, c) } }
 }
